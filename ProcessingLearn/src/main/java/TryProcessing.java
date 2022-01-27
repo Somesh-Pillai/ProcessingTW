e67a55b3c1046b81bd84c1ed1e50e6e0f7844b77
Somesh-Pillai <57011503+Somesh-Pillai@users.noreply.github.com>
2022-01-27 15:38:58 +0530

    Add files via upload

---
+++ ProcessingLearn/src/main/java/TryProcessing.java
@@ -0,0 +1,54 @@
+import processing.core.PApplet;
+
+public class TryProcessing extends PApplet {
+
+
+    public static final int WIDTH = 648;
+    public static final int HEIGHT = 488;
+    public static final int DIAMETER = 10;
+    int s1, s2, s3, s4 = 0 ;
+    public static void main(String[] args){
+        PApplet.main("TryProcessing", args);
+    }
+    @Override
+    public void settings() {
+        super.settings();
+        size(WIDTH, HEIGHT);
+    }
+
+    @Override
+    public void setup() { //called once
+//        ellipse(WIDTH/2, HEIGHT/2, 100,100 );           //(x, y, height width)
+
+    }
+
+    @Override
+    public void draw() {   // infinite call
+//        printWhite();
+        drawCircle1();
+        drawCircle2();
+        drawCircle3();
+        drawCircle4();
+    }
+
+    private void drawCircle1() {
+        ellipse(s1, HEIGHT/5, DIAMETER,DIAMETER);
+        s1++;
+    }
+    private void drawCircle2() {
+        ellipse(s2, 2 * HEIGHT/5, DIAMETER,DIAMETER);
+        s2 += 2;
+    }
+    private void drawCircle3() {
+        ellipse(s3, 3 * HEIGHT/5, DIAMETER,DIAMETER);
+        s3 += 3;
+    }
+    private void drawCircle4() {
+        ellipse(s4, 4 * HEIGHT/5, DIAMETER,DIAMETER);
+        s4 += 4;
+    }
+
+    private void printWhite() {
+        background(255);                             //erasing every time we draw by white
+    }
+}
